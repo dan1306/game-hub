efa601680d2f1fd08d504f9ea47dcc43bdc5fd00
dan1306 <81488150+dan1306@users.noreply.github.com>
2023-06-17 17:09:42 -0400

    display critic sscore

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Game } from "../hooks/userGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { PlatformIconList } from "./PlatformIconList";
+import { CriticScore } from "./CriticScore";
 
 interface Props {
     game: Game
 }
 
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
 
     return (
         <Card borderRadius={10} fontSize={'2xl'} overflow={'hidden'}>
@@ -16,10 +17,13 @@ const GameCard = ({game}: Props) => {
                 <Heading>
                     {game.name}
                 </Heading>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+                <HStack justifyContent={'space-between'}>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
             </CardBody>
         </Card>
-    )    
+    )
 }
 
 export default GameCard
--- src/hooks/userGames.tsx
+++ src/hooks/userGames.tsx
@@ -6,14 +6,15 @@ import { CanceledError } from "axios";
 export interface Platform {
     id: number;
     name: string;
-    slug: string
+    slug: string,
 }
 
 export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platforms: { platform: Platform}[]
+    parent_platforms: { platform: Platform }[];
+    metacritic: number
 }
 
 interface FetchGameResponse {
